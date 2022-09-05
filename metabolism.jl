f8eaf01b97eefb18e7d21b7e251e9764a7a19f89
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-09-06 02:04:47 +0300

    Download metabolic model and map

--- metabolism.jl
+++ metabolism.jl
@@ -5,3 +5,11 @@ using CairoMakie
 using Tulip
 using Colors
 ##
+
+##
+# Download a toy model of Escherichia coli's central metabolism.
+download("http://bigg.ucsd.edu/static/models/e_coli_core.json", "e_coli_core.json")
+
+# Download the metabolic map. 
+download("http://bigg.ucsd.edu/escher_map_json/e_coli_core.Core%20metabolism", "e_coli_core_map.json")
+##
