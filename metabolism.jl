c8e06f46e5c291cba76afd51dcbef4509093c9fd
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-09-06 02:46:37 +0300

    Add metabolism visualization util

--- metabolism.jl
+++ metabolism.jl
@@ -24,6 +24,7 @@ function constructpath(filename)
     return joinpath(filename)
 end
 
+# Save to IO.
 function writeio(data, dirname, filename)
     open(constructpath(dirname, filename), "w") do io
       write(io, data)
@@ -33,29 +34,40 @@ function writeio(data, dirname, filename)
 ##
 
 ##
+# Visualize the metabolism.
+function vismetabolism(mappath)
+    escherplot(
+        mappath; 
+        reaction_show_text = true,
+        reaction_edge_color = :grey,
+        metabolite_show_text = true,
+        metabolite_node_colors = Dict("glc__D_e" => :red),
+        metabolite_node_color = :lightskyblue,
+    )
+
+    hidexdecorations!(current_axis())
+    hideydecorations!(current_axis())
+
+    return current_figure()
+end
+##
+
+##
+# Global paths.
+const MODELPATH = constructpath("data", "e_coli_core.json")
+const MAPPATH = constructpath("data", "e_coli_core_map.json")
+const METABOLISMPATH = constructpath("results", "metabolism.pdf")
+##
+
+##
 # Download a toy model of Escherichia coli's central metabolism.
-Downloads.download("http://bigg.ucsd.edu/static/models/e_coli_core.json", constructpath("data", "e_coli_core.json"))
+Downloads.download("http://bigg.ucsd.edu/static/models/e_coli_core.json", MODELPATH)
 
 # Download the metabolic map. 
-Downloads.download("http://bigg.ucsd.edu/escher_map_json/e_coli_core.Core%20metabolism", constructpath("data", "e_coli_core_map.json"))
-##
-
-##
-# Visualize the metabolism.
-escherplot(
-    constructpath("data", "e_coli_core_map.json"); 
-    reaction_show_text = true,
-    reaction_edge_color = :grey,
-    metabolite_show_text = true,
-    metabolite_node_colors = Dict("glc__D_e" => :red),
-    metabolite_node_color = :lightskyblue,
-)
-hidexdecorations!(current_axis())
-hideydecorations!(current_axis())
-p = current_figure()
+Downloads.download("http://bigg.ucsd.edu/escher_map_json/e_coli_core.Core%20metabolism", MAPPATH)
 ##
 
 ##
 # Save metabolism graph.
-save(constructpath("results", "metabolism.pdf"), p)
+save(METABOLISMPATH, vismetabolism(MAPPATH))
 ##
