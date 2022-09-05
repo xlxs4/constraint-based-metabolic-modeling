c0ba3e07af5bbbc589cfaa68569a8eb87f084ac6
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-09-06 02:06:17 +0300

    Plot metabolism

--- metabolism.jl
+++ metabolism.jl
@@ -13,3 +13,18 @@ download("http://bigg.ucsd.edu/static/models/e_coli_core.json", "e_coli_core.jso
 # Download the metabolic map. 
 download("http://bigg.ucsd.edu/escher_map_json/e_coli_core.Core%20metabolism", "e_coli_core_map.json")
 ##
+
+##
+# Visualize the metabolism.
+escherplot(
+    "e_coli_core_map.json"; 
+    reaction_show_text = true,
+    reaction_edge_color = :grey,
+    metabolite_show_text = true,
+    metabolite_node_colors = Dict("glc__D_e" => :red),
+    metabolite_node_color = :lightskyblue,
+)
+hidexdecorations!(current_axis())
+hideydecorations!(current_axis())
+current_figure()
+##
