425eed1d30a3933b774e06e182059081c9e77868
Orestis Ousoultzoglou <orousoultzoglou@gmail.com>
2022-09-06 16:45:12 +0300

    Refactor

--- metabolism.jl
+++ metabolism.jl
@@ -35,30 +35,30 @@ function writeio(data, dirname, filename)
 
 ##
 # Visualize the metabolism.
-function vismetabolism(mappath, reactionedgecolor)
-    function _escherplot(reactionedgecolor::Symbol)
+function vismetabolism(mappath, reaction_edge_color)
+    function _escherplot(reaction_edge_color::Symbol)
         return escherplot(
             mappath; 
             reaction_show_text = true,
-            reaction_edge_color = reactionedgecolor,
+            reaction_edge_color = reaction_edge_color,
             metabolite_show_text = true,
             metabolite_node_colors = Dict("glc__D_e" => :red),
             metabolite_node_color = :lightskyblue,
         )
     end
 
-    function _escherplot(reactionedgecolors::AbstractDict)
+    function _escherplot(reaction_edge_colors::AbstractDict)
         return escherplot(
             mappath; 
             reaction_show_text = true,
-            reaction_edge_colors = reactionedgecolors,
+            reaction_edge_colors = reaction_edge_colors,
             metabolite_show_text = true,
             metabolite_node_colors = Dict("glc__D_e" => :red),
             metabolite_node_color = :lightskyblue,
         )
     end
 
-    _escherplot(reactionedgecolor)
+    _escherplot(reaction_edge_color)
 
     hidexdecorations!(current_axis())
     hideydecorations!(current_axis())
@@ -69,28 +69,28 @@ end
 
 ##
 # Global paths.
-const MODELPATH = constructpath("data", "e_coli_core.json")
-const MAPPATH = constructpath("data", "e_coli_core_map.json")
-const METABOLISMPATH = constructpath("results", "metabolism.pdf")
-const DEFREACTIONSPATH = constructpath("results", "default_reactions.pdf")
+const ModelPath = constructpath("data", "e_coli_core.json")
+const MapPath = constructpath("data", "e_coli_core_map.json")
+const MetabolismPath = constructpath("results", "metabolism.pdf")
+const DefReactionsPath = constructpath("results", "default_reactions.pdf")
 ##
 
 ##
 # Download a toy model of Escherichia coli's central metabolism.
-Downloads.download("http://bigg.ucsd.edu/static/models/e_coli_core.json", MODELPATH)
+Downloads.download("http://bigg.ucsd.edu/static/models/e_coli_core.json", ModelPath)
 
 # Download the metabolic map. 
-Downloads.download("http://bigg.ucsd.edu/escher_map_json/e_coli_core.Core%20metabolism", MAPPATH)
+Downloads.download("http://bigg.ucsd.edu/escher_map_json/e_coli_core.Core%20metabolism", MapPath)
 ##
 
 ##
 # Save metabolism graph.
-save(METABOLISMPATH, vismetabolism(MAPPATH, :grey))
+save(MetabolismPath, vismetabolism(MapPath, :grey))
 ##
 
 ##
 # Simulate E. coli growth.
-const model = load_model(MODELPATH)
+const model = load_model(ModelPath)
 # Flux Balance Analysis (FBA).
 const fluxes = flux_balance_analysis_dict(model, Tulip.Optimizer) # flux_summary(fluxes)
 ##
@@ -98,6 +98,6 @@ const fluxes = flux_balance_analysis_dict(model, Tulip.Optimizer) # flux_summary
 ##
 # Save control metabolic reactions graph.
 const tolerance = 1e-3
-const reactionedgecolors = Dict(id => :red for (id, flux) ∈ fluxes if abs(flux) > tolerance)
-save(DEFREACTIONSPATH, vismetabolism(MAPPATH, reactionedgecolors))
+const reaction_edge_colors = Dict(id => :red for (id, flux) ∈ fluxes if abs(flux) > tolerance)
+save(DefReactionsPath, vismetabolism(MapPath, reaction_edge_colors))
 ##
